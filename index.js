81b70b8d94fe30c7e4c7e077277c67f264024dc6
manoti <manotimike@gmail.com>
2023-03-17 11:23:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array) {
+return array.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(array) {
+
+return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(fare) {
+    return function(dist) {
+        return fare * dist
+    } 
+}
+const calcFare = createFareMultiplier(3)
+
+
+function fareDoubler(num) {
+    return num * 2
+}
+
+function fareTripler(num) {
+    return num * 3
+}
+
+function selectDifferentDrivers(driverparam, funcParam) {
+    let selectedDriver = funcParam(driverparam)
+    return selectedDriver
+}
+
+
